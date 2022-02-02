9a0ed77ca67ca9eb51f069a8492195122e6a8a28
Marson Souza <marsonorlandisouza2502@hotmail.com>
2022-02-02 11:02:58 -0300

    produtos

---
+++ admin-products.php
@@ -0,0 +1,115 @@
+<?php
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Product;
+
+$app->get("/admin/products", function()
+{
+    User::verifyLogin();
+    
+    $search = (isset($_GET['search'])) ? $_GET['search']    : '';
+	$page   = (isset($_GET['page']))   ? (int)$_GET['page'] : 1;
+
+	if(trim($search) != '')
+	{
+		$pagination = Product::getPageSearch($search, $page);
+	}
+	else
+	{
+		$pagination = Product::getPage($page);
+	}
+
+	$pages = [];
+
+	for ($i=0; $i < $pagination['pages']; $i++)
+	{ 
+		array_push($pages, [
+			'href'=>'/admin/categories?'.http_build_query([
+				'page'=>$i+1,
+				'search'=>$search
+			]),
+			'text'=>$i+1
+		]);
+	}
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products", [
+        "products"=>$pagination['data'],
+        "search"  =>$search,
+        "pages"   =>$pages
+    ]);
+});
+
+$app->get("/admin/products/create", function() 
+{
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+	$page->setTpl("products-create");
+});
+
+$app->post("/admin/products/create", function() 
+{
+    User::verifyLogin();
+
+    $product = new Product();
+
+    $product->setData($_POST);
+
+    $product->save();
+
+    header("Location: /admin/products");
+    exit;
+});
+
+$app->get("/admin/products/:idproduct", function($idproduct) 
+{
+    User::verifyLogin();
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $page = new PageAdmin();
+    
+    $page->setTpl("products-update", array(
+        "product" => $product->getValues()
+    ));
+});
+
+$app->post("/admin/products/:idproduct", function($idproduct) 
+{
+    User::verifyLogin();
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $product->setData($_POST);
+
+    $product->save();
+
+    $product->setPhoto($_FILES["file"]);
+
+    header("Location: /admin/products");
+    exit;
+
+});
+
+$app->get("/admin/products/:idproduct/delete", function($idproduct) 
+{
+    User::verifyLogin();
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+    
+    $product->delete();
+
+    header("Location: /admin/products");
+    exit;
+});
+?>
--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php 
+session_start();
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-	echo "OK";
+require_once("functions.php");
 
-});
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-orders.php");
+require_once("admin-products.php");
 
 $app->run();
-
- ?>
+?>
