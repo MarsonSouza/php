5ef149a69689533baa8cca2e03b716c0f805d355
Marson Souza <marsonorlandisouza2502@hotmail.com>
2022-02-02 13:56:28 -0300

    mudarqtd

--- site.php
+++ site.php
@@ -66,8 +66,6 @@ $app->get("/products/:desurl", function($desurl)
 	]);
 });
 
-/* Carrinho de Compras ------------------------------------------------------- */
-
 $app->get("/cart", function() 
 {
 	$cart = Cart::getFromSession();
@@ -315,8 +313,6 @@ $app->get('/order/:idorder/paypal', function($idorder)
 	]);
 });
 
-/* Login de usuário do site ------------------------------------------------------------------ */
-
 $app->get('/login', function()
 {
 	$page = new Page();
@@ -356,8 +352,6 @@ $app->get('/logout', function()
 	header("Location: /login");
 	exit;
 });
-
-/* Cadastro de Usuário do Site ------------------------------------------------------- */
 
 $app->post('/register', function()
 {
@@ -417,8 +411,6 @@ $app->post('/register', function()
 	exit;
 });
 
-/* Recuperar Senha --------------------------------------------------------------------*/
-
 $app->get("/forgot", function() {
 	$page = new Page();
 	$page->setTpl("forgot");
@@ -468,9 +460,6 @@ $app->post("/forgot/reset", function() {
 
 	$page->setTpl("forgot-reset-success");
 });
-
-
-/* Minha Conta ----------------------------------------------------------------- */
 
 $app->get("/profile", function()
 {
@@ -526,7 +515,6 @@ $app->post("/profile", function()
 
 	$user->update(true);
 
-	//Atualiza variaveis da sessão
 	$_SESSION[User::SESSION]['deslogin']  = $_POST['desemail'];
 	$_SESSION[User::SESSION]['desperson'] = $_POST['desperson'];
 	$_SESSION[User::SESSION]['desemail']  = $_POST['desemail'];
@@ -561,28 +549,25 @@ $app->get('/boleto/:idorder', function($idorder)
 
 	$order->get((int)$idorder);
 
-	// DADOS DO BOLETO PARA O SEU CLIENTE
 	$dias_de_prazo_para_pagamento = 10;
 	$taxa_boleto = 5.00;
-	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-	$valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  
+	$valor_cobrado = formatPrice($order->getvltotal()); 
 	$valor_cobrado = str_replace(".", "",$valor_cobrado);
 	$valor_cobrado = str_replace(",", ".",$valor_cobrado);
 	$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
-	$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
-	$dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
-	$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-	$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
-	$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
-	$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+	$dadosboleto["nosso_numero"] = $order->getidorder();  
+	$dadosboleto["numero_documento"] = $order->getidorder();	
+	$dadosboleto["data_vencimento"] = $data_venc; 
+	$dadosboleto["data_documento"] = date("d/m/Y"); 
+	$dadosboleto["data_processamento"] = date("d/m/Y"); 
+	$dadosboleto["valor_boleto"] = $valor_boleto; 	
 
-	// DADOS DO SEU CLIENTE
 	$dadosboleto["sacado"] = $order->getdesperson();
 	$dadosboleto["endereco1"] = $order->getdesaddress()." ".$order->getdesdisctric();
 	$dadosboleto["endereco2"] = $order->getdescity()." ".$order->getdesstate()." - ".$order->getdescountry()." - CEP: ".$order->getdeszipcode();
 
-	// INFORMACOES PARA O CLIENTE
 	$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Hcode E-commerce";
 	$dadosboleto["demonstrativo2"] = "Taxa bancária - R$ 0,00";
 	$dadosboleto["demonstrativo3"] = "";
@@ -591,33 +576,24 @@ $app->get('/boleto/:idorder', function($idorder)
 	$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@hcode.com.br";
 	$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto Loja Hcode E-commerce - www.hcode.com.br";
 
-	// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
 	$dadosboleto["quantidade"] = "";
 	$dadosboleto["valor_unitario"] = "";
 	$dadosboleto["aceite"] = "";		
 	$dadosboleto["especie"] = "R$";
 	$dadosboleto["especie_doc"] = "";
-
-
-	// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
-
-
-	// DADOS DA SUA CONTA - ITAÚ
+	
 	$dadosboleto["agencia"] = "1690"; // Num da agencia, sem digito
 	$dadosboleto["conta"] = "48781";	// Num da conta, sem digito
 	$dadosboleto["conta_dv"] = "2"; 	// Digito do Num da conta
 
-	// DADOS PERSONALIZADOS - ITAÚ
 	$dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
 
-	// SEUS DADOS
 	$dadosboleto["identificacao"] = "Hcode Treinamentos";
 	$dadosboleto["cpf_cnpj"] = "24.700.731/0001-08";
 	$dadosboleto["endereco"] = "Rua Ademar Saraiva Leão, 234 - Alvarenga, 09853-120";
 	$dadosboleto["cidade_uf"] = "São Bernardo do Campo - SP";
 	$dadosboleto["cedente"] = "HCODE TREINAMENTOS LTDA - ME";
 
-	// NÃO ALTERAR!
 	$path = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."res".DIRECTORY_SEPARATOR."boletophp".DIRECTORY_SEPARATOR."include".DIRECTORY_SEPARATOR;
 
 	require_once($path."funcoes_itau.php");
